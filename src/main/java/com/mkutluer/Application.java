14de88fa46287321e988162276e90e6ef96c57ff
Murat Kutluer <murat.kutluer@kartaca.com>
2020-10-09 19:56:47 +0300

    Initial commit.

---
+++ src/main/java/com/mkutluer/Application.java
@@ -0,0 +1,20 @@
+package com.mkutluer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class Application {
+
+    @RequestMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
